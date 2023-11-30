6735ad82c7dbb912205d7a4cda7dcbf6b7e42722
Gerep <daniel.gerep@skiff.com>
2023-11-30 08:29:48 -0300

    11

--- scenes/basic_enemy/basic_enemy.gd
+++ scenes/basic_enemy/basic_enemy.gd
@@ -3,7 +3,7 @@ extends CharacterBody2D
 const MAX_SPEED = 75
 
 func _ready():
-	pass
+	$Area2D.area_entered.connect(on_area_entered)
 
 
 func _process(delta):
@@ -18,3 +18,7 @@ func get_direction():
 		return (player_node.global_position - global_position).normalized()
 
 	return Vector2.ZERO
+
+
+func on_area_entered(other_area: Area2D):
+	queue_free()
