52f5951fc81f67f83ce35d3877ea0758aced17de
Gerep <daniel.gerep@skiff.com>
2023-12-11 08:18:47 -0300

    28

---
+++ scenes/UI/victory_screen.gd
@@ -0,0 +1,15 @@
+extends CanvasLayer
+
+func _ready():
+	get_tree().paused = true
+	%Restart.pressed.connect(on_restart_pressed)
+	%Quit.pressed.connect(on_quit_pressed)
+
+
+func on_restart_pressed():
+	get_tree().paused = false
+	get_tree().change_scene_to_file("res://scenes/main/main.tscn")
+
+
+func on_quit_pressed():
+	get_tree().quit()
