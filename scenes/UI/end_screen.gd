9e195e6572b0dc4ee72a98a2bb83fd7a9f7445ce
Gerep <daniel.gerep@skiff.com>
2023-12-11 08:46:16 -0300

    29

--- scenes/UI/end_screen.gd
+++ scenes/UI/end_screen.gd
@@ -11,5 +11,10 @@ func on_restart_pressed():
 	get_tree().change_scene_to_file("res://scenes/main/main.tscn")
 
 
+func set_defeat():
+	%TitleLabel.text = "Defeat"
+	%DescriptionLabel.text = "You lost!"
+
+
 func on_quit_pressed():
 	get_tree().quit()
